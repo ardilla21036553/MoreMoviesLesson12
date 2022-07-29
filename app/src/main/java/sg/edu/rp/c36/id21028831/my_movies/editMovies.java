e3e3621a3cde9961b9f170dd6e02e5e014807928
Matthew21028831 <21028831@myrp.edu.sg>
2022-07-29 17:47:04 +0800

    Matthew sixth commit

---
+++ app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/editMovies.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c36.id21028831.my_movies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_movies);
+    }
+}
--- app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/showMovies.java
+++ app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/showMovies.java
@@ -2,7 +2,10 @@ package sg.edu.rp.c36.id21028831.my_movies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -23,10 +26,20 @@ public class showMovies extends AppCompatActivity {
         caMovies= new CustomAdapter(this, R.layout.row, al);
         lv.setAdapter(caMovies);
 
-        DBHelper dbh=new DBHelper(showMovies.this);
-        al.clear();
-        al.addAll(dbh.getAllMovies());
-        caMovies.notifyDataSetChanged();
+//        DBHelper dbh=new DBHelper(showMovies.this);
+//        al.clear();
+//        al.addAll(dbh.getAllMovies());
+//        caMovies.notifyDataSetChanged();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Movies data=al.get(position);
+                Intent i=new Intent(showMovies.this, editMovies.class);
+                i.putExtra("data", data);
+                startActivity(i);
+            }
+        });
 
     }
 }
