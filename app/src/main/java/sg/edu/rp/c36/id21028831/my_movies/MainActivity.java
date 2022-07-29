6d5c9bc686ef2a450a0e451218579d5fb8345d81
Matthew21028831 <21028831@myrp.edu.sg>
2022-07-29 15:02:00 +0800

    Matthew first commit

--- app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/MainActivity.java
@@ -3,12 +3,22 @@ package sg.edu.rp.c36.id21028831.my_movies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etTitle;
+    EditText etGenre;
+    EditText etYear;
+    Spinner spinnerRating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etTitle=findViewById(R.id.etTitle);
+        etGenre=findViewById(R.id.etGenre);
+        etYear=findViewById(R.id.etYear);
+        spinnerRating=findViewById(R.id.spinnerRating);
+
     }
 }
