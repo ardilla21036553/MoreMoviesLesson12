3b82f28e17b95da5bd3230b3a002be770e237275
Ardilla <21036553@myrp.edu.sg>
2022-07-29 15:41:28 +0800

    dbhelper

--- app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/Movies.java
+++ app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/Movies.java
@@ -7,7 +7,7 @@ public class Movies implements Serializable {
     private String genre;
     private int year;
     private int rating;
-
+//help
     public Movies(String title, String genre, int year, int rating) {
 
         this.title = title;
@@ -16,5 +16,39 @@ public class Movies implements Serializable {
         this.rating = rating;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString(){ return  title + "\n" + genre + "\n" + year + "\n" + rating; }
 
 }
