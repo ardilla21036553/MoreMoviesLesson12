cafacbd8e739385507eacb68b823fe17fed1e9b2
Ardilla <21036553@myrp.edu.sg>
2022-07-30 01:04:37 +0800

    ard's third commit

--- app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/CustomAdapter.java
+++ app/src/main/java/sg/edu/rp/c36/id21028831/my_movies/CustomAdapter.java
@@ -37,19 +37,35 @@ public class CustomAdapter extends ArrayAdapter {
         tvGenre.setText(currentItem.getGenre());
         tvYear.setText(currentItem.getYear()+"");
 
-        if (currentItem.getRating()=="G"){
+//        if (currentItem.getRating()=="G"){
+//            ivRating.setImageResource(R.drawable.rating_g);
+//        }else if (currentItem.getRating()=="PG"){
+//            ivRating.setImageResource(R.drawable.rating_pg);
+//        }else if (currentItem.getRating()=="PG13"){
+//            ivRating.setImageResource(R.drawable.rating_pg13);
+//        }else if (currentItem.getRating()=="NC16"){
+//            ivRating.setImageResource(R.drawable.rating_nc16);
+//        }else if (currentItem.getRating()=="M18"){
+//            ivRating.setImageResource(R.drawable.rating_m18);
+//        }else if (currentItem.getRating()=="R21"){
+//            ivRating.setImageResource(R.drawable.rating_r21);
+//        }
+//equals
+        if(currentItem.getRating().equals("G")  ){
             ivRating.setImageResource(R.drawable.rating_g);
-        }else if (currentItem.getRating()=="PG"){
+        } else if(currentItem.getRating().equals("PG") ){
             ivRating.setImageResource(R.drawable.rating_pg);
-        }else if (currentItem.getRating()=="PG13"){
+        } else if(currentItem.getRating().equals("PG13")){
             ivRating.setImageResource(R.drawable.rating_pg13);
-        }else if (currentItem.getRating()=="NC16"){
+        } else if(currentItem.getRating().equals("NC16")){
             ivRating.setImageResource(R.drawable.rating_nc16);
-        }else if (currentItem.getRating()=="M18"){
+        } else if(currentItem.getRating().equals("M18")){
             ivRating.setImageResource(R.drawable.rating_m18);
-        }else if (currentItem.getRating()=="R21"){
+        } else if(currentItem.getRating().equals("R21")){
             ivRating.setImageResource(R.drawable.rating_r21);
         }
+
+
 
         return rowView;
     }
